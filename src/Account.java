d3f99ffefa5cc0302638af00ccf95bac753450a8
luizcordista <luizcordista@gmail.com>
2024-04-22 18:48:20 -0300

    Adicionar classes Account e User

---
+++ src/Account.java
@@ -0,0 +1,55 @@
+import java.math.BigDecimal;
+
+public class Account {
+    double balance;
+    String owner;
+
+    public Account(String owner) {
+        this.balance = 0.00;
+        this.owner = owner;
+    }
+
+    public Account(String owner, double balance) {
+        this.balance = balance;
+        this.owner = owner;
+    }
+
+    public void withDraw(double amount) {
+        if (this.balance >= amount) {
+            this.balance -= amount;
+            System.out.println("Your money has been withdrawn");
+        }
+        else {
+            System.out.println("You don't have this amount");
+        }
+    }
+
+    public void deposit(double amount) {
+        this.balance += amount;
+    }
+
+    public String getStatus() {
+        return "Your name: " + this.owner + "\nYour balance = " + this.balance;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        if (balance < 0) {
+            System.out.println("Balance only accepts positive numbers");
+        }
+        else {
+            this.balance = balance;
+        }
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+}
